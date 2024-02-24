e78bc8036f9cdf3fb714639949ad36cfbfa2a444
codeonoto <onoto0409@gmail.com>
2024-02-24 09:31:17 +0530

    creating swaggers (api-docs)

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 // 1. Import Express
 import express from 'express';
+import swagger from 'swagger-ui-express';
+
 import productRouter from './src/features/product/product.routes.js';
 import userRouter from './src/features/user/user.routes.js';
 import bodyParser from 'body-parser';
 import jwtAuth from './src/middlewares/jwt.middleware.js';
 import cartRouter from './src/features/cart/cartItems.routes.js';
+import apiDocs from './swagger.json' assert { type: 'json' };
 
 // 2. Create a Server
 const server = express();
@@ -12,6 +15,8 @@ const server = express();
 server.use(bodyParser.json());
 
 // for all requests related to product, redirect to product routes
+// swaggerUI
+server.use('/api-docs', swagger.serve, swagger.setup(apiDocs));
 // localhost:3200/api/products
 server.use('/api/products', jwtAuth, productRouter);
 // localhost:3200/api/users
